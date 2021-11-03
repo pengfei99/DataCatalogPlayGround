e7a4b43ee28a2dc368deb080167bfcd77812e119
pengfei <liu.pengfei@hotmail.fr>
2021-11-03 16:29:56 +0000

    add hive table and process lineage

--- Atlas/metadata-loader/submit_request.sh
+++ Atlas/metadata-loader/submit_request.sh
@@ -23,28 +23,42 @@ url2="https://atlas.lab.sspcloud.fr/api/atlas/v2"
 
 
 # get all entities of type hive_table
-curl -iv -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/entities?type=hive_table"
+# curl -iv -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/entities?type=hive_table"
 
+# get all entities of type hive_process
+# curl -iv -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/entities?type=hive_process"
+
+
+# get details of an entity by its guid
+# guid for test2 e1a3fb3c-321e-4441-b147-d1e5fde32021
+# guid for test c5ee6a58-d381-483f-9794-6f511d79cacf
+# guid for hive_process1 "ca5cc018-0893-4eb6-9a76-248833ee3a5f"
+# guid for hive_process2 "e207f6f5-3470-4cdf-b073-7251ae0571a3"
+guid="ca5cc018-0893-4eb6-9a76-248833ee3a5f"
+curl -iv -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/entity/guid/${guid}"
 
 ############################## Insert example ############################################################
 # upload the typedefs in the json file
-# curl -iv -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./typedef-node.json"
+# curl -iv -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./metadata_source/typedef-node.json"
 
 # ok
-# curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./typedef-file.json"
+# curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./metadata_source/typedef-file.json"
 
 # ok
-# curl -negotiate -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./typedef-kafka_with_schema.json"
+# curl -negotiate -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./metadata_source/typedef-kafka_with_schema.json"
 
-# failed
-# curl -iv -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/types/typedefs" -d "@./typedef-python_process.json"
+# ok
+# curl -iv -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/types/typedefs" -d "@./metadata_source/typedef-python_process.json"
 
-# failed
-# curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/entity/bulk" -d "@./infrastructure-entities.json"
+# ok
+# curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/entity/bulk" -d "@./metadata_source/infrastructure-entities.json"
 
-# failed
-# curl -negotiate -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/entity/bulk" -d "@./news-ingestion-dataset.json"
+# ok
+# curl -negotiate -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/entity/bulk" -d "@./metadata_source/news-ingestion-dataset.json"
 
-# 
-# curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url/entity/bulk" -d "@./news-ingestion-process.json"
+# curl -negotiate -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/entity/bulk" -d "@./metadata_source/news-ingestion-process.json"
+
+
+# ingest hive table/process
+# curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${oidc_token}" "$url2/entity/bulk" -d "@./metadata_source/hive_table_entities_ingestion.json"
 
